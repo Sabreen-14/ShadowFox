5d4eb60b87cb1d2a08636dca86340469885322c2
sabreen <sabreen14083@gmail.com>
2026-04-12 11:28:11 +0530

    Update Task1_Variables.py

--- beginner/Task1_Variables.py
+++ beginner/Task1_Variables.py
@@ -1,1 +1,25 @@
+# ----------- TASK 1: VARIABLES (WITH USER INPUT) -----------
 
+# PART 1: Value of PI
+pi = 22 / 7
+print("Value of pi:", pi)
+print("Datatype of pi:", type(pi))
+
+
+# PART 2: Reserved Keyword Test
+
+
+
+for_value = int(input("\nEnter a value for variable: "))
+print("Valid variable (for_value):", for_value)
+
+
+# PART 3: Simple Interest
+
+principal = float(input("\nEnter principal amount: "))
+rate = float(input("Enter rate of interest: "))
+time = float(input("Enter time (in years): "))
+
+simple_interest = (principal * rate * time) / 100
+
+print("Simple Interest:", simple_interest)
